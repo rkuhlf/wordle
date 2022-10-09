2d327b8eaabc63931b43f6d722d1c5a044e5e3e9
Riley <rileyk42@icloud.com>
2022-10-09 16:31:47 -0500

    Re-factored analysis to be more functional

--- wordListAnalysis.py
+++ wordListAnalysis.py
@@ -1,7 +1,19 @@
-words = open("realwords.csv", "r").read().split()
 
-for word in words:
-    for letter in "abcdefghijklmnopqrstuvqxyzQWERTYUIOPASDFGHJKLZXCVBNM":
-        if word.count(letter) >= 3:
-            print(word)
-            break
+def load_words(path):
+    words = open(path, "r").read().split()
+
+
+def print_duplicated_letter_words(words, min_duplicated=3):
+    for word in words:
+        for letter in "abcdefghijklmnopqrstuvqxyzQWERTYUIOPASDFGHJKLZXCVBNM":
+            if word.count(letter) >= min_duplicated:
+                print(word)
+                # If the word is five letters and it has three duplicates, then we no longer need to search for duplicates.
+                if min_duplicated >= 3:
+                    break
+            
+
+if __name__ == "__main__":
+    words = load_words("realwords.csv")
+    
+    print_duplicated_letter_words(words)
